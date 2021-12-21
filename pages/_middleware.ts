3cbe49f57d2a638718a8bb194881e71594dac3df
= <=>
2021-12-20 22:44:22 -0500

    added longitude and latitude

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -5,10 +5,14 @@ export async function middleware(req: NextRequest) {
   const country = geo?.country || "US";
   const city = geo?.city || "San Francisco";
   const region = geo?.region || "CA";
+  const latitude = geo?.latitude || 37.7749;
+  const longitude = geo?.longitude || 122.4194;
 
   url.searchParams.set("country", country);
   url.searchParams.set("city", city);
   url.searchParams.set("region", region);
+  url.searchParams.set("lat", latitude.toString());
+  url.searchParams.set("lon", longitude.toString());
 
   return NextResponse.rewrite(url);
 }
