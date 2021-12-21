b770f9b3ab8795b0b8627457231ddcf8fe6346b5
= <=>
2021-12-21 01:29:54 -0500

    remove unused fields

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,15 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const { nextUrl: url, geo } = req;
-  const country = geo?.country || "US";
   const city = geo?.city || "San Francisco";
-  const region = geo?.region || "CA";
   const latitude = geo?.latitude || 37.7749;
   const longitude = geo?.longitude || 122.4194;
 
-  url.searchParams.set("country", country);
   url.searchParams.set("city", city);
-  url.searchParams.set("region", region);
   url.searchParams.set("lat", latitude.toString());
   url.searchParams.set("lon", longitude.toString());
 
